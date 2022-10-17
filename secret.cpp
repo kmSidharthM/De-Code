30c9a1ab32d83037df140bd5f6f534b4e1846763
Sidharth Murali <kmsidharthmurali@gmail.com>
2022-10-18 02:36:53 +0530

    Third commit

---
+++ secret.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<ctype.h>
+using namespace std;
+
+int main()
+{
+    int ch,m,n,flag=1;
+    char text[100];
+    int secCode[100];
+    cout<<"\n\n************************************DE-CODE************************************\n\n";
+    while(flag==1)
+    {
+    cout<<"WHAT TO DO:\n1->CODE\n2->DECODE\n";
+    cin>>ch;
+    if(ch==1)
+    {   
+        cout<<"ENTER THE TEXT:";
+        cin.ignore();
+        gets(text);
+        cout<<"\nTHE CODE IS READY.......\n";
+        for(int i=0;text[i]!='\0';i++)
+            cout<<(int)toupper(text[i])-9;
+    }
+    else if(ch==2)
+    {
+        cout<<"ENTER THE CODE:";
+        cin.ignore();
+        gets(text);
+        int a;
+        cout<<"THE SECRET TEXT IS READY........\n";
+        for(int i=0;text[i]!='\0';i+=2)
+        {   
+        m=text[i]-'0';
+        n=text[i+1]-'0';
+        a=m*10+n;
+        cout<<(char)(tolower(a+9));
+        }
+    }
+    cout<<"\nDO YOU WANT TO CONTINUE(1/0):";
+    cin>>flag;
+    }
+    cout<<"\n\n************************************THANKYOU************************************\n\n";
+    return 0;
+}
